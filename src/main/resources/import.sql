0fad4e90da14ece88d08b38abd53eb4fc65cf6d2
marcelodelabarrera <marcelo.delabarrera@api-ux.com>
2022-04-07 16:14:25 -0400

    Add roles to test user

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -61,3 +61,5 @@ INSERT INTO users (username, password, enabled) VALUES ('test-user','$2a$10$/eOQ
 INSERT INTO authorities (user_id, authority) VALUES (1,'ROLE_USER');
 INSERT INTO authorities (user_id, authority) VALUES (2,'ROLE_ADMIN');
 INSERT INTO authorities (user_id, authority) VALUES (2,'ROLE_USER');
+INSERT INTO authorities (user_id, authority) VALUES (3,'ROLE_ADMIN');
+INSERT INTO authorities (user_id, authority) VALUES (3,'ROLE_USER');
